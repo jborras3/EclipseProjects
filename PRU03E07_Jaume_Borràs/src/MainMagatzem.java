aa441ef224239584ab31799332a8b541895858b6
jborras3 <jborras3@nigul.cide.es>
2020-01-08 20:44:21 +0100

    8/1/2020

---
+++ PRU03E07_Jaume_Borràs/src/MainMagatzem.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class MainMagatzem {
+
+	public static void main(String[] args) {
+		
+		static int selector = 0;
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("'Menu': eligeix una opcio:");
+		System.out.println("");
+		System.out.println("1. Llistat");
+		System.out.println("2. Alta");
+		System.out.println("3. Baixa");
+		System.out.println("4. Modificació");
+		System.out.println("5. Entrada de mercaderia");
+		System.out.println("6. Sortida de mercaderia");
+		System.out.println("7. Sortir");
+		
+		selector = s.nextInt();
+		
+		switch (selector) {
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			case 7:
+				break;
+			
+			default:
+				break;
+		}
+		
+	}
+
+}
