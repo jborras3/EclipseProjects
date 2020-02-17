b2891f2b0b18b4124a344305e1379386b9871740
jborras3 <jborras3@nigul.cide.es>
2020-02-17 19:51:54 +0100

    Stream ex1

--- StreamJaumeBorras/src/MainStream.java
+++ StreamJaumeBorras/src/MainStream.java
@@ -1,63 +1,92 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class MainStream {
 
-	public static void main(String[] args){
-		
-		String ubicacionFichero = "C:\\Users\\jborras3\\Desktop\\pitufo.txt"; //ubicacion del arrxiu
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+
+		String ubicacionFichero = "C:\\Users\\usuario\\Desktop\\pitufo.txt"; // ubicacion del arrxiu
 		int size = 5;
 		String[] data = new String[size];
-		char[] paraula1 = new char[11];
-		char[] paraula2 = new char[11];
+		String aux;
 		int countLector = 0;
-		int countChar = 0;
-		int countParaula = 0;
-		char tmp;
-		
-		System.out.println("Comenca");
-		
-			try
-	        {
-	            BufferedReader lector = new BufferedReader(new FileReader(ubicacionFichero)); //declarar lector
-	            
-	            System.out.println("Arxiu obert");
-	            System.out.println(" ");
-	            
-	            while(countLector != 5) {
-	            	
-	            	data[countLector] = lector.readLine();
-	            	
-	            	System.out.println(data[countLector]);
-	            	
-	            	countLector++;
-	            	
-	            }
-	            
-	            lector.close();
-	            
-	            System.out.println(" ");
-	            
+
+		System.out.println("Inserti ruta absoluta del document a ordenar: (recorda que les barres son dues)");
+		ubicacionFichero = s.next();
+
+
+
+		System.out.println("Comença");
+
+		try {
+			BufferedReader lector = new BufferedReader(new FileReader(ubicacionFichero)); // declarar lector
+
+			System.out.println("Arxiu obert");
+			System.out.println(" ");
+
+			try{
+				while (countLector != 5) { //llegir lineas del fitxar
+
+					data[countLector] = lector.readLine();
+	
+					System.out.println(data[countLector]);
+	
+					countLector++;
+	
+				}
+			}catch(Exception e){
+				System.out.println("Hi ha poques lines");
+			}
+			
+			
+			lector.close(); //cerrar lector
+
+			for (int i = 0; i < (data.length - 1); i++) { //algoritme de ordenació
+
+				for (int j = 0; j < data.length - i - 1; j++) {
+
+					char[] primerString = Procesos.convertirStringToChar(data[j]); //transformar string a array de chars
+					char[] segundoString = Procesos.convertirStringToChar(data[j + 1]);
+
+					if (Procesos.comparadorChar(primerString, segundoString)) { //metode de validació si es mes gran o no
+						aux = data[j + 1];
+						data[j + 1] = data[j];
+						data[j] = aux;
+					}
+				}
+			}
+
 		} catch (FileNotFoundException e) { // que fer si no troba el fitxer
 			System.out.println("No es troba el fitxer");
 		} catch (IOException e) { // que fer si hi ha un error en la lectura del fitxer
 			System.out.println("No es pot llegir");
-		}	
-			
-		System.out.println("Acabat");
-		
-				//hacer lo de ethan porfa pls
-		
-		while(countPalabra != 5) {
-			
 		}
-		
-			
+		System.out.println(" ");
+		System.out.println("Acabat");
+		System.out.println(" ");
+		try {
+			BufferedWriter escritor = new BufferedWriter(new FileWriter("C:\\Users\\usuario\\Desktop\\words_sort.txt"));
+
+			for (int o = 0; o < data.length; o++) {
+				escritor.write(data[o] + "\n");
+				System.out.println(data[o]);
+			}
+
+			escritor.close();
+
+		} catch (IOException e) {
+			System.out.println("Ruta erronea");
+		}
+
+		s.close();
+
 	}
-		
-	
+
 }
-
-
--- StreamJaumeBorras/src/Procesos.java
+++ StreamJaumeBorras/src/Procesos.java
@@ -1,7 +1,45 @@
-
 public class Procesos {
-	
-	
-	
 
+    static int comprovador;
+    static boolean validor;
+    static char[] charArray;
+    static int count = 0;
+        
+	public static char[] convertirStringToChar(String a) { //String to charArray
+        charArray = a.toCharArray(); 
+        return charArray;
+    }
+    
+
+    public static boolean comparadorChar(char[] primerString, char[] segundoString) {
+     try {
+
+        do{
+        
+            if (primerString[count] > segundoString[count]) { //si 1 es més gran que dos = true (que se intercabin)
+                comprovador = 2;
+                validor = true;
+            }
+            if( primerString[count] < segundoString[count]){ //si 2 es més gran que dos = false (que no se intercabin)
+                comprovador = 1;
+                validor = false;
+            }
+            if( primerString[count] == segundoString[count]){ //si les lletres son iguals es passa a la seguent
+                comprovador = 0;
+            }
+
+
+            count++;
+
+        }while(comprovador == 0);
+
+        count = 0;
+        
+            } catch (Exception e) { //ern cas que siguin molt iguals i un arriba a null s'alta la excepcio
+            System.out.println("Una de les dues paraules son massa iguals");
+        }
+        
+     return validor;
+    
+    }
 }
