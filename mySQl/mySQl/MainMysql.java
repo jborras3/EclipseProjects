1a5d4ae949e2bd1281d9fd8a75f7db4e558aabb1
jborras3 <jborras3@nigul.cide.es>
2020-03-13 17:42:46 +0100

    mysql Connect

---
+++ mySQl/mySQl/MainMysql.java
@@ -0,0 +1,9 @@
+
+public class MainMysql {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ mySQl/src/mySQl/Main.java
@@ -0,0 +1,10 @@
+package mySQl;
+import java.
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
